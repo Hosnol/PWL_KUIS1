379e9cd10f3a4970debde62f4a9429eedc3744c9
Hosnol Arifin <hosnolarifin220@gmail.com>
2021-03-10 13:58:35 +0700

    membuat view untuk barang,pelanggan,pegawai,suplai

---
+++ resources/views/barang.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card-body">
+    <table class="table table-bordered">
+        <tr>
+            <th></th>
+        </tr>
+
+        @foreach()
+            <tr>
+                <td></td>
+            </tr>
+        @endforeach
+    </table>
+</div>
+@endsection
